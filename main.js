9bba10c57317df6826cae7b199db3f7f30774bd1
Hichaocau <91059288+Hichaocau@users.noreply.github.com>
2021-12-02 10:39:17 +0700

    upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var test = document.querySelector('.footer__bonus')
+// console.log([test])
+
+// test.style.width = '100px',
+// test.style.color = 'red',
+// test.style.backgroundColor = 'red',
+// ===
+// nhớ dấu phảy sau style
+Object.assign(test.style, {
+    witdh :'100px',
+    color: 'red',
+    backgroundColor: 'red'
+});
